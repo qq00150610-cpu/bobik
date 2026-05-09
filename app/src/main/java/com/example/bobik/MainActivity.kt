ac9d75d6da92b0d425873277d97542af63be08ec
qq00150610-cpu <qq00150610@163.com>
2026-05-09 22:16:21 +0800

    feat: 创建项目文件

---
+++ app/src/main/java/com/example/bobik/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bobik
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.bobik.navigation.AppNavigation
+import com.example.bobik.ui.theme.BobikTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BobikTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
